35908ac37d05a4bf7f37dc96d18dd75f8085f165
hunama <hunama@github.com>
2023-07-03 14:50:16 +0900

    add evolution monster project

--- app/src/main/java/semonster2/Monster.java
+++ app/src/main/java/semonster2/Monster.java
@@ -5,13 +5,18 @@ public class Monster {
   int rare;// 1:normal,2:uncommon,3:rare,4:ultra rare
 
   Monster(int nameNum, int rareNum) {
-    this.name = this.summonMonster(nameNum);
+    this.name = this.summonMonster(nameNum, rareNum);
     this.rare = rareNum;
   }
 
-  String summonMonster(int mnumber) {
+  String summonMonster(int mnumber, int mrare) {
     String monsters[] = { "スライム", "サハギン", "ドラゴン", "デュラハン", "シーサーペント" };
-    return monsters[mnumber];
+    String evolutionMonsters[] = { "キングスライム", "キングサハギン", "キングドラゴン", "キングデュラハン", "キングシーサーペント" };
+    if (mrare >= 3) {
+      return evolutionMonsters[mnumber];
+    } else {
+      return monsters[mnumber];
+    }
   }
 
   @Override
